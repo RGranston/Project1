5c2be50845a87f306169223c62bd6fc34f3a86d5
RGranston <ryan.granston@gmail.com>
2022-09-14 17:25:45 -0700

    screen2fix

--- screener2.py
+++ screener2.py
@@ -9,9 +9,7 @@ yf.pdr_override()
 stock = input("Enter a stock ticker symbol:")
 
 print(stock)
-startyear=2022
-startmonth=1
-startday=1
+
 
 start=dt.datetime.now() - dt.timedelta(days=365)
 end=dt.datetime.now()
