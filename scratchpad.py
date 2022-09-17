eeec731ae8ec8dec6121fd65c001697edc052496
unknown <yu.taka.070202@gmail.com>
2022-09-17 02:26:23 -0700

    deleted scratchpad.py

--- scratchpad.py
+++
@@ -1,9 +0,0 @@
-import questionary
-
-# Ask user what type of technical analysis they're looking for
-def ask_indicators():
-    technical_analysis_indicators = [
-        "Golden Cross", "Dead Cross", "20EMA and 20SMA", "RSI", "exit"
-    ]
-    result = questionary.select("Which indicator would you like to use to filter?", technical_analysis_indicators).ask()
-    return result
