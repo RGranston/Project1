359bad8476aafcb677eeb4e9a2fb269fc1391465
Yu Takahashi <yu.taka.070202@gmail.com>
2022-09-20 21:47:02 -0700

    change screener2.py to use yahoo_fin

--- references/screener2.py
+++ references/screener2.py
@@ -1,19 +1,13 @@
-import pandas as pd
-import numpy as np
-import yfinance as yf
+from yahoo_fin import stock_info as si
 import datetime as dt
-from pandas_datareader import data as pdr
-
-yf.pdr_override()
 
 stock = input("Enter a stock ticker symbol:")
 
 print(stock)
 
-
 start=dt.datetime.now() - dt.timedelta(days=365)
 end=dt.datetime.now()
 
-df=pdr.get_data_yahoo(stock,start,end)
+df=si.get_data(stock,start,end)
 
 print(df)
